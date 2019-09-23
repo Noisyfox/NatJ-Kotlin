ded64459480c943acc451a65222ccec667e2521d
Noisyfox <timemanager.rick@gmail.com>
2019-09-23 14:59:43 +1000

    Make bridge functions callable by Java

--- lib/src/main/kotlin/io/noisyfox/moe/natj/Bridge.kt
+++ lib/src/main/kotlin/io/noisyfox/moe/natj/Bridge.kt
@@ -9,25 +9,49 @@ import org.moe.natj.objc.ObjCObject
 import org.moe.natj.objc.ObjCRuntime
 
 /** __bridge transfers a pointer between Objective-C and Core Foundation with no transfer of ownership. */
-inline fun <reified T : OpaquePtr> ObjCObject.bridge(): T = ObjCRuntime.cast(this, T::class.java)
+fun <T : OpaquePtr> bridge(obj: ObjCObject, type: Class<T>): T = ObjCRuntime.cast(obj, type)
+
+/** __bridge transfers a pointer between Objective-C and Core Foundation with no transfer of ownership. */
+inline fun <reified T : OpaquePtr> ObjCObject.bridge(): T = bridge(this, T::class.java)
 
 /**
  * __bridge transfers a pointer between Objective-C and Core Foundation with no transfer of ownership.
  *
  * You still need to release the original CFType instance if it's owned by you afterwards.
  */
-inline fun <reified T : ObjCObject> OpaquePtr.bridge(): T = ObjCRuntime.cast(this, T::class.java)
+fun <T : ObjCObject> bridge(ptr: OpaquePtr, type: Class<T>): T = ObjCRuntime.cast(ptr, type)
+
+/**
+ * __bridge transfers a pointer between Objective-C and Core Foundation with no transfer of ownership.
+ *
+ * You still need to release the original CFType instance if it's owned by you afterwards.
+ */
+inline fun <reified T : ObjCObject> OpaquePtr.bridge(): T = bridge(this, T::class.java)
 
 /**
  * __bridge_retained or CFBridgingRetain casts an Objective-C pointer to a Core Foundation pointer and
  * also transfers ownership to you.
  * You are responsible for calling CFRelease or a related function to relinquish ownership of the object.
  */
-inline fun <reified T : OpaquePtr> ObjCObject.bridgeRetained(): T = CRuntime.cast(Foundation.CFBridgingRetain(this), T::class.java)
+fun <T : OpaquePtr> bridgeRetained(obj: ObjCObject, type: Class<T>): T = CRuntime.cast(Foundation.CFBridgingRetain(obj), type)
+
+/**
+ * __bridge_retained or CFBridgingRetain casts an Objective-C pointer to a Core Foundation pointer and
+ * also transfers ownership to you.
+ * You are responsible for calling CFRelease or a related function to relinquish ownership of the object.
+ */
+inline fun <reified T : OpaquePtr> ObjCObject.bridgeRetained(): T = bridgeRetained(this, T::class.java)
 
 /**
  * __bridge_transfer or CFBridgingRelease moves a non-Objective-C pointer to Objective-C and also transfers
  * ownership to ARC.
  * ARC is responsible for relinquishing ownership of the object.
  */
-inline fun <reified T : ObjCObject> OpaquePtr.bridgeTransfer(): T = Foundation.CFBridgingRelease(this) as T
+fun <T : ObjCObject> bridgeTransfer(ptr: OpaquePtr, type: Class<T>): T = ObjCRuntime.cast(Foundation.CFBridgingRelease(ptr), type)
+
+/**
+ * __bridge_transfer or CFBridgingRelease moves a non-Objective-C pointer to Objective-C and also transfers
+ * ownership to ARC.
+ * ARC is responsible for relinquishing ownership of the object.
+ */
+inline fun <reified T : ObjCObject> OpaquePtr.bridgeTransfer(): T = bridgeTransfer(this, T::class.java)
